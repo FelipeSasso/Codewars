3eb01adaf6c20d6af75b9e2ed6be1ffdb60ed4a1
Felipe Coral Sasso <FelipeSasso@users.noreply.github.com>
2017-04-22 09:14:01 -0400

    Update TwoToOne.py

--- TwoToOne.py
+++ TwoToOne.py
@@ -1,7 +1,6 @@
 __author__ = "Felipe Coral Sasso"
 
 """
-
 Description:
 
 Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters, - each taken only once - coming from s1 or s2.
@@ -14,7 +13,6 @@ longest(a, b) -> "abcdefklmopqwxy"
 
 a = "abcdefghijklmnopqrstuvwxyz"
 longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
-
 """
 
 def longest(s1, s2):
